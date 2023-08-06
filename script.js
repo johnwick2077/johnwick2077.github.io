7e5d02a7bb5b893731197dd76a44954108905291
johnwick2077 <70083433+johnwick2077@users.noreply.github.com>
2023-08-06 06:06:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
-// Smooth scrolling functionality for navigation links
+// Smooth scrolling functionality for navigation links and Back to Top button
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
+        const target = document.querySelector(this.getAttribute('href'));
+        const headerOffset = 100; // Adjust the offset as needed (consider the height of the header)
+
+        // Calculate the target position and scroll smoothly
+        const targetPosition = target.getBoundingClientRect().top + window.pageYOffset - headerOffset;
+        window.scroll({
+            top: targetPosition,
             behavior: 'smooth'
         });
     });
 });
 
 // Show the "Back to Top" button when scrolling down
+let isScrolling = false;
+
 window.addEventListener('scroll', function () {
-    if (window.pageYOffset > 100) {
-        document.querySelector('.back-to-top').style.display = 'block';
-    } else {
-        document.querySelector('.back-to-top').style.display = 'none';
-    }
+    isScrolling = true;
+    throttleScroll(); // Throttle the scroll event to improve performance
 });
+
+function throttleScroll() {
+    if (isScrolling) {
+        requestAnimationFrame(function () {
+            showBackToTop();
+            isScrolling = false;
+        });
+    }
+}
+
+function showBackToTop() {
+    const backToTopButton = document.querySelector('.back-to-top');
+    if (window.pageYOffset > 100) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+}
 
 // Add comment functionality here to allow visitors to leave comments on blog posts
 // You can use a backend server or a third-party commenting system for this purpose
